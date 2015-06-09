c0a0527fa44d9d38b574e286537d3338e1bac5e3
Freeman10 <adam.rozboril98@gmail.com>
2015-06-09 18:08:39 +0200

    Přidán ovladač pro RONJu

---
+++ RONJADriver/RONJADriver/Program.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Threading;
+
+namespace RONJADriver
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			if (args.Length != 0)
+			{
+				if (args.Length == 3)
+				{
+					Choice(Convert.ToChar(args[0]), args[1], Convert.ToInt32(args[2]));
+				}
+				else
+				{
+					Choice(Convert.ToChar(args[0]), args[1]);
+				}
+			}
+			string port = null;
+			char mode;
+			ConsoleKeyInfo cki;
+			Console.Write("Port: ");
+			port = Console.ReadLine();
+			Console.Write("Mode[R,T]: ");
+			cki = Console.ReadKey();
+			mode = Char.ToUpper(cki.KeyChar);
+			Console.WriteLine();
+            try
+            {
+                Choice(mode, port, 10);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Oops!");
+                Console.WriteLine(e.StackTrace);
+            }
+		}
+		private static void Choice(char mode, string port, int lines)
+		{
+			switch (mode)
+			{
+				case 'R':
+					{
+						SerialReader rx = new SerialReader(port);
+						rx.PrintData(lines);
+						break;
+					}
+				case 'T':
+					{
+						SerialWriter tx = new SerialWriter(port);
+						tx.SendTypedData();
+						break;
+					}
+				default:
+					{
+						throw new ArgumentOutOfRangeException("mode", "Can only be T or R");
+					}
+			}
+		}
+		private static void Choice(char mode, string port)
+		{
+			switch (mode)
+			{
+				case 'R':
+					{
+						SerialReader rx = new SerialReader(port);
+						rx.PrintData();
+						break;
+					}
+				case 'T':
+					{
+						SerialWriter tx = new SerialWriter(port);
+						tx.SendTypedData();
+						break;
+					}
+				default:
+					{
+						throw new ArgumentOutOfRangeException("mode", "Can only be T or R");
+					}
+			}
+		}
+	}
+}
