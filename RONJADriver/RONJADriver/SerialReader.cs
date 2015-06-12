655d238e48fe895f348c3eaf211f95bb9502ea6f
Roman3349 <ondracek.roman@centrum.cz>
2015-06-12 06:11:01 +0200

    Formát kódu driveru

--- RONJADriver/RONJADriver/SerialReader.cs
+++ RONJADriver/RONJADriver/SerialReader.cs
@@ -4,57 +4,59 @@ using System.Threading;
 
 namespace RONJADriver
 {
-	public class SerialReader  //V budoucnu spojit se SerialWriterem()
+	// V budoucnu spojit se SerialWriterem()
+	public class SerialReader
 	{
-        const int baudrate = 4800;  //Přenosová rychlost pro PICAxe 08M2
-		public SerialReader(string port)
+		// Přenosová rychlost pro PICAxe 08M2
+		const int baudrate = 4800;
+
+		public SerialReader (string port)
 		{
-			Port = new SerialPort(port, baudrate, Parity.None);
-            if (Port.IsOpen == false)
-            {
-                Console.WriteLine("Opening port {0}, baud rate: {1}", port, baudrate);
-                Port.Open();
-            }
-            else
-            {
-                if (Port.BaudRate != 4800)
-                {
-                    throw new ApplicationException("Baud rate is " + Port.BaudRate + " and should be " + baudrate);  //Pokud je port otevřen, ale není na správné přenosové rychlosti, bude program dělat neplechu
-                }
-            }
-            Console.WriteLine("Port {0} opened!", port); 
+			Port = new SerialPort (port, baudrate, Parity.None);
+			if (Port.IsOpen == false) {
+				Console.WriteLine ("Opening port {0}, baud rate: {1}", port, baudrate);
+				Port.Open ();
+			} else {
+				if (Port.BaudRate != 4800) {
+					// Pokud je port otevřen, ale není na správné přenosové rychlosti, bude program dělat neplechu
+					throw new ApplicationException ("Baud rate is " + Port.BaudRate + " and should be " + baudrate);  
+				}
+			}
+			Console.WriteLine ("Port {0} opened!", port); 
 		}
-		private SerialPort Port
-		{
+
+		private SerialPort Port {
 			get;
 			set;
 		}
-        private void Open()  //Jen pro pohodlí
-        {
-            if (Port.IsOpen == false)
-            {
-                Console.WriteLine("Opening port {0}", Port.PortName);
-            }
-            Console.WriteLine("Port {0} opened!", Port.PortName);
-        }
-		public string GetData() //Příjem dat, důvod této třídy. V budoucích verzích sloučit se SerialWriterem()
+		// Jen pro pohodlí
+		private void Open ()
+		{
+			if (Port.IsOpen == false) {
+				Console.WriteLine ("Opening port {0}", Port.PortName);
+			}
+			Console.WriteLine ("Port {0} opened!", Port.PortName);
+		}
+		// Příjem dat, důvod této třídy. V budoucích verzích sloučit se SerialWriterem()
+		public string GetData ()
 		{
 			Thread.Sleep (300);
-			return Port.ReadLine();
+			return Port.ReadLine ();
 		}
-		public void PrintData(int lines)  //Příjem dat a jejich následné tištění přímo na konzolový výstup
+		// Příjem dat a jejich následné tištění přímo na konzolový výstup
+		public void PrintData (int lines)
 		{
 			int line = 0;
-			while (lines > line)
-			{
-				Console.WriteLine("RX: {0}", RONJACoder.GetMessage(GetData()));
-				Thread.Sleep(100);
+			while (lines > line) {
+				Console.WriteLine ("RX: {0}", RONJACoder.GetMessage (GetData ()));
+				Thread.Sleep (100);
 				line++;
 			}
 		}
-		public void PrintData()  //To samé, jen s jedním řádkem
+		// To samé, jen s jedním řádkem
+		public void PrintData ()
 		{
-            Console.WriteLine("RX: {0}", RONJACoder.GetMessage(GetData()));
+			Console.WriteLine ("RX: {0}", RONJACoder.GetMessage (GetData ()));
 		}
 	}
 }
--- RONJADriver/RONJADriver/SerialWriter.cs
+++ RONJADriver/RONJADriver/SerialWriter.cs
@@ -4,63 +4,56 @@ using System.IO.Ports;
 
 namespace RONJADriver
 {
-    public class SerialWriter
-    {
-        public SerialWriter(string port)
-        {
-            Port = new SerialPort(port, 4800, Parity.None, 8);
-            if (Port.IsOpen == false)
-            {
-                Port.Open();
-            }
-        }
-        private SerialPort Port
-        {
-            get;
-            set;
-        }
-        public void SendData(string data)
-        {
-            char[] letters = data.ToCharArray();
-            foreach (char letter in letters)
-            {
-                Port.Write(letter.ToString());
-				Thread.Sleep(300);
-            }
-            Port.WriteLine("");
-        }
-        public void SendTypedData()
-        {
-            bool exit = false;
-            while (true)
-            {
-                string data = null;
-                Console.Write("TX: ");
-                data = Console.ReadLine();
-                switch(data)
-                {
-                    case "/quit":  //Konec
-                        {
-                            exit = true;
-                            break;
-                        }
-                    /*case "/rx":  //Skok na jeden řádek přijímání
-                        {
-                            
-                            break;
-                        }*/
-                    default:  //Poslání dat
-                        {
-                            SendData(data);
-                            break;
-                        }
-                }
-                if (exit == true)
-                { 
-                    break;
-                }
-            }
-        }
-    }
+	public class SerialWriter
+	{
+		public SerialWriter (string port)
+		{
+			Port = new SerialPort (port, 4800, Parity.None, 8);
+			if (Port.IsOpen == false) {
+				Port.Open ();
+			}
+		}
+
+		private SerialPort Port {
+			get;
+			set;
+		}
+
+		public void SendData (string data)
+		{
+			char[] letters = data.ToCharArray ();
+			foreach (char letter in letters) {
+				Port.Write (letter.ToString ());
+				Thread.Sleep (300);
+			}
+			Port.WriteLine ("");
+		}
+
+		public void SendTypedData ()
+		{
+			bool exit = false;
+			while (true) {
+				string data = null;
+				Console.Write ("TX: ");
+				data = Console.ReadLine ();
+				switch (data) {
+				// Konec
+				case "/quit":  
+					exit = true;
+					break;
+				// Skok na jeden řádek přijímání
+				/*case "/rx":
+                	break;*/
+				// Poslání dat
+				default:
+					SendData (data);
+					break;
+				}
+				if (exit == true) { 
+					break;
+				}
+			}
+		}
+	}
 }
 
